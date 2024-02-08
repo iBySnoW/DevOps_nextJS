6a5939c4761410691b6aa431b1b95a2a3fb3ab3a
Corentin Danvin <corentindanvin@gmail.com>
2024-02-08 15:16:29 +0100

    new style

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,3 +1,8 @@
 export default function Page() {
-    return <h1>Hello from blog racine page</h1>
-  }
+     return (
+          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+               <h1>Hello from blog racine page</h1>
+               <p>LoremIpsum</p>
+          </main>
+     );
+}
